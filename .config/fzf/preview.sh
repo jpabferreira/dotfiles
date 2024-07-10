48d1d097c6cddaa45a3e0dee83178fc3e4628106
João Ferreira <jpabferreira@gmail.com>
2024-07-10 15:39:32 +0100

    Update FZF previewer
    - Refactor
    - Bugfixes

--- .config/fzf/preview.sh
+++ .config/fzf/preview.sh
@@ -1,70 +1,143 @@
 #!/bin/bash
+#
+# FZF previewer for the Kitty terminal, supporting the following file types:
+# - Image
+# - Video (takes a still frame)
+# - PDF (generates a cover image)
+# - Font files (generates an image sample)
+# - Archive files (plays audio and shows metadata)
+# - Torrent files
+# - Text files / other (previewed with bat)
+#
+# Depends on:
+# - kitty w/ nerd font, fzf, bat, lsd, pdftoppm, als, transmission, id3info
+#
+# Use as:
+# $ fzf --preview='/path/to/preview.sh {}'
+#
 
 if [[ $# -ne 1 ]]; then
   >&2 echo "usage: $0 FILENAME"
   exit 1
 fi
 
-fname=${1/#\~\//$HOME/}
-mime=$(xdg-mime query filetype "$fname")
-# mime=$(file -b --mime-type "$fname")
+# Print a title on top of the FZF preview window
+# Arguments:
+# - Title (string)
+title() {
+		echo "$@"
+		printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
+		echo ""
+}
 
-if echo ${mime} | grep -qF image/; then
-	echo "  Image preview:" $(basename "$fname")
-	printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-	echo ""
-	kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=$((FZF_PREVIEW_COLUMNS))x$((FZF_PREVIEW_LINES - 2))@0x0 "$fname" | sed '$d' | sed $'$s/$/\e[m/'
-elif echo ${mime} | grep -qF video/; then
-	TMP=$(mktemp)
-	echo "  Video preview:" $(basename "$fname")
-	printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-	echo ""
-	ffmpegthumbnailer -i "$fname" -o $TMP -s 0 && kitty icat --clear --transfer-mode=memory ---unicode-placeholder -stdin=no --place=$((FZF_PREVIEW_COLUMNS))x$((FZF_PREVIEW_LINES - 2))@0x0 $TMP | sed '$d' | sed $'$s/$/\e[m/'
-	trap "rm -f $TMP" EXIT
-elif echo ${mime} | grep -qE "font/|application/vnd.ms-fontobject|application/vnd.ms-opentype|application/vnd.oasis.opendocument.formula-template"; then
-	echo "  Font preview:" $(basename "$fname")
-	printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-	echo ""
-	TMP=$(mktemp /tmp/XXXXXXXX.jpg)
-	convert -size 800x500 xc:"#2B2B2B" -fill "#E6E1DC" -gravity center -pointsize 35 -font "$fname" -annotate +0+0 "ABCDEFGHIJKLM\nNOPQRSTUVWXYZ\nabcdefghijklm\nnopqrstuvwxyz\n1234567890\n!@$\%(){ }[]\n𝅘𝅥𝅮⁋ℑ𝝨⌛^E↋↺𝒴≸\n◔✌😱☎🌍🥷🩻🚳🜹\n𐆖₿⛧🨁🂡⮘🮕🯵‱" -flatten $TMP
-	kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=$((FZF_PREVIEW_COLUMNS))x$((FZF_PREVIEW_LINES - 2))@0x0 "$TMP" | sed '$d' | sed $'$s/$/\e[m/'
-	trap "rm -f $TMP" EXIT
+# Show an image in the FZF preview pane using Kitty's icat
+# Arguments:
+# - File path (string)
+show-img() {
+	kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=$((FZF_PREVIEW_COLUMNS))x$((FZF_PREVIEW_LINES - 2))@0x0 "$@" | sed '$d' | sed $'$s/$/\e[m/'
+}
+
+# Regexes to match mime types
+MIME_IMG="image/"
+MIME_VID="video/"
+MIME_TOR="/x-bittorrent"
+MIME_PDF="/pdf"
+MIME_FON="font/|application/vnd.ms-fontobject|application/vnd.ms-opentype|application/vnd.oasis.opendocument.formula-template"
+MIME_ARC="/zip|/gzip|/x-xz|/x-bzip|/x-rar|/gzip|/x-7z-compressed"
+
+# Text to generate font preview
+TEXT_SAMPLE="ABCDEFGHIJKLM\nNOPQRSTUVWXYZ\nabcdefghijklm\nnopqrstuvwxyz\n1234567890\n!@$\%(){ }[]\n𝅘𝅥𝅮⁋ℑ𝝨⌛^E↋↺𝒴≸\n◔✌😱☎🌍🥷🩻🚳🜹\n𐆖₿⛧🨁🂡⮘🮕🯵‱"
+
+# File path
+retfile="$1"
+# File name
+fname=$(basename "$retfile")
+# Directory path
+dpath=$(pwd)
+# File path
+fpath="$dpath/$retfile"
+
+# Temporary file (preview) path
+#
+# A preview image for /path/to/file.ext is created in /tmp/path/to/file.ext.jpg
+# This ensures that each file in our system has a unique correspondance in /tmp
+# and enables us to reuse a previously generated preview. Things will go wrong
+# if the original file is changed or replaced between previews.
+# /tmp content is flushed between Linux sessions.
+#
+# TODO: Compare timestamps (preview file creation vs. original file
+# modification) to update the preview if the original file has changed.
+#
+tfpath="/tmp$fpath.jpg"
+# Temporary directory (preview) path
+tdpath=$(dirname "$tfpath")
+
+# Get the mime type
+mime=$(xdg-mime query filetype "$fpath")
+# Alternative:
+# mime=$(file -b --mime-type "$fpath")
+
+# Make the temporary directory, if needed
+if [[ ! -d $tdpath ]]; then
+	mkdir -p "$tdpath"
+fi
+
+if echo "${mime}" | grep -qF "$MIME_IMG"; then
+	title "  Image preview: $fname"
+
+	# Preview image directly
+	show-img "$fpath" || echo "  Error previewing image."
+elif echo "${mime}" | grep -qF "$MIME_VID"; then
+	ok=true
+
+	title "  Video preview: $fname"
+	if [[ ! -s $tfpath ]]; then
+		ffmpegthumbnailer -i "$fpath" -o "$tfpath" -s 0 || ok=false
+	fi
+
+	# Show generated image
+	$ok && show-img "$tfpath" || echo "  Error generating video preview."
+elif echo "${mime}" | grep -qE "$MIME_FON"; then
+	ok=true
+	title "  Font preview: $fname"
+	if [[ ! -s $tfpath ]]; then
+		touch "$tfpath"
+		convert -size 800x500 xc:"#2B2B2B" -fill "#E6E1DC" -gravity center -pointsize 35 -font "$fpath" -annotate +0+0 "$TEXT_SAMPLE" -flatten "$tfpath" || ok=false;
+	fi
+
+	# Show generated image
+	$ok && show-img "$tfpath" || echo "  Error generating font preview."
 else
-	if [[ -d "$fname" ]]; then
-		echo "󱞊  Directory preview"
-		printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-		echo ""
-		lsd --icon=always --color=always --tree --depth 1 "$fname" | less
-	elif echo ${mime} | grep -qF /pdf; then
-		echo "  PDF preview:" $(basename "$fname")
-		printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-		echo ""
-		pdftoppm -f 1 -l 1 -scale-to 1024 -jpeg -jpegopt quality=100 "$fname" /tmp/pdfprev
-		kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=$((FZF_PREVIEW_COLUMNS))x$((FZF_PREVIEW_LINES - 2))@0x0 "/tmp/pdfprev-01.jpg" | sed '$d' | sed $'$s/$/\e[m/'
-		trap "rm -f /tmp/pdfprev-01.jpg" EXIT
-	elif echo ${mime} | grep -qE "/zip|/gzip|/x-xz|/x-bzip|/x-rar|/gzip|/x-7z-compressed"; then
-		echo "  Archive preview:" $(basename "$fname")
-		printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-		echo ""
-		als "$fname" 2>/dev/null
-	elif echo ${mime} | grep -qE "/x-bittorrent"; then
-		echo "  Torrent preview:" $(basename "$fname")
-		printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-		echo ""
-		transmission-show "$fname"
-	# elif echo ${mime} | grep -qF audio/mpeg; then
-	# 	echo "  ID3 Info:" $(basename "$fname")
-	# 	printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-	# 	id3info "$fname"
-	elif echo ${mime} | grep -qF audio/; then
-		echo "  Audio preview:" $(basename "$fname")
-		printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-		id3info "$fname"
-		ffplay "$fname" -nodisp -loglevel quiet
+	if [[ -d "$fpath" ]]; then
+		title "󱞊  Directory preview"
+		lsd --icon=always --color=always --tree --depth 1 "$fpath" | less
+	elif echo "${mime}" | grep -qF /pdf; then
+		ok=true
+
+		title "  PDF preview: $fname"
+		if [[ ! -s $tfpath ]]; then
+			pdftoppm -f 1 -l 1 -scale-to 1024 -jpeg -jpegopt quality=100 "$fpath" "$tfpath" || ok=false
+		fi
+
+		# Show generated image
+		$ok && show-img "$tfpath" || echo "  Error generating PDF preview."
+	elif echo "${mime}" | grep -qE "$MIME_ARC"; then
+		title "  Archive preview: $fname"
+		als "$fpath" 2>/dev/null
+	elif echo "${mime}" | grep -qE "$MIME_TOR"; then
+		title "  Torrent preview: $fname"
+		# Show torrent file details
+		transmission-show "$fpath"
+	elif echo "${mime}" | grep -qF audio/; then
+		title "  Audio preview: $fname"
+		# Show ID3 data
+		id3info "$fpath"
+		# Play audio
+		ffplay "$fpath" -nodisp -loglevel quiet
 	else
-		echo "  File preview:" $(basename "$fname")
-		printf "%.0s─" $(seq $FZF_PREVIEW_COLUMNS)
-		echo ""
-		bat --style=numbers --color=always "$fname"
+		title "  File preview: $fname"
+
+		# Try to read the file with bat
+		bat --style=numbers --color=always "$fpath"
 	fi
 fi
